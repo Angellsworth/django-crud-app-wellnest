7c637b4cb0594dd271c5dc3de44d1b4929a0a21f
Angellsworth <angellsworth@msn.com>
2025-04-28 19:41:28 -0600

    wip: one last thing

--- wellnest_project/settings.py
+++ wellnest_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uxauh1^(+5d8vqu1yzay5npmju@(c$l5ux4(=f6+ept!z(kj=@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
